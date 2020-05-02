7b1c519125ea3cad45a1710389cfd9324df36ef6
t_kirifu <uniuniworld777@gmail.comu>
2020-05-02 22:55:08 +0900

    no message

---
+++ s2/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Takahiro Kirifu on 2020/05/02.
+//  Copyright © 2020 Takahiro Kirifu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
